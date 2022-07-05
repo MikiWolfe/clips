9a1cd39e9001391fd7bcd6376c1497c4b49db865
Mikayla Bruce <89227020+MikiWolfe@users.noreply.github.com>
2022-07-05 15:44:27 -0700

    intial commit for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAWwKrnVruZClYB2tu4bTWKZmz7szf2ptE',
+    authDomain: 'clips-99433.firebaseapp.com',
+    projectId: 'clips-99433',
+    storageBucket: 'clips-99433.appspot.com',
+    appId: '1:940108023757:web:a3593c08024233dfa0e684',
+  },
+};
